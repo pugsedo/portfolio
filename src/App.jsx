fc8bda06f03ca5efe3ee8cf92e08b4ebe25262cf
Mikhail <mmikeb72@proton.me>
2023-09-30 21:38:42 -0700

    Added Navigation Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import NavBar from "./components/navbar";
+
 import LandingPage from "./components/landing";
 import ProjectsPage from "./components/projects";
 
 function App() {
   return (
     <div className="app">
+      <NavBar/>
       <LandingPage/>
       <ProjectsPage/>
     </div>
